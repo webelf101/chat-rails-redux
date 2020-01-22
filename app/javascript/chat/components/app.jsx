78f9ccf6cadcbe65304fa4802e2d2a37cd09a017
webelf000 <m63974529@gmail.com>
2020-01-22 20:20:12 +0900

    passing the channel props from router to channel list and message list

--- app/javascript/chat/components/app.jsx
+++ app/javascript/chat/components/app.jsx
@@ -2,16 +2,19 @@ import React from 'react';
 import ChannelList from '../containers/channel_list';
 import MessageList from '../containers/message_list';
 
-const App = () => {
-  return (
-    <div className="messaging-wrapper">
-      <div className="logo-container">
-        <img className="messaging-logo" src="" alt="logo" />
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="messaging-wrapper">
+        <div className="logo-container">
+          <img className="messaging-logo" src="" alt="logo" />
+        </div>
+        <ChannelList selectedChannel={this.props.match.params.channel} />
+        <MessageList selectedChannel={this.props.match.params.channel} />
       </div>
-      <ChannelList />
-      <MessageList />
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default App;
